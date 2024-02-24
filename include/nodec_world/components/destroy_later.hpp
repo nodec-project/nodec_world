3910a936224798b1cb06c008c4a463262600ffc6
IOE <fivetwothreesix@gmail.com>
2024-02-24 19:21:49 +0900

    feat: Add DestroyLater component

---
+++ include/nodec_world/components/destroy_later.hpp
@@ -0,0 +1,17 @@
+#ifndef NODEC_WORLD__COMPONENTS__DESTROY_LATER_HPP_
+#define NODEC_WORLD__COMPONENTS__DESTROY_LATER_HPP_
+
+namespace nodec_world {
+namespace components {
+
+/**
+ * @brief The component to mark an entity to be destroyed in end of the step. 
+ * 
+ */
+struct DestroyLater {
+};
+
+} // namespace components
+} // namespace nodec_world
+
+#endif
--- include/nodec_world/impl/world_impl.hpp
+++ include/nodec_world/impl/world_impl.hpp
@@ -1,6 +1,7 @@
 #ifndef NODEC_WORLD__IMPL__WORLD_IMPL_HPP_
 #define NODEC_WORLD__IMPL__WORLD_IMPL_HPP_
 
+#include "../components/destroy_later.hpp"
 #include "../world.hpp"
 
 namespace nodec_world {
@@ -15,17 +16,26 @@ public:
     void step() {
         clock_.tick();
         stepped_(*this);
+        on_end_of_step();
     }
 
     void step(float delta_sec) {
         clock_.tick(delta_sec);
         stepped_(*this);
+        on_end_of_step();
     }
 
     void reset() {
         scene().registry().clear();
         clock_.reset();
         initialized_(*this);
+    }
+
+private:
+    void on_end_of_step() {
+        auto &scene_registry = scene().registry();
+        auto view = scene_registry.view<components::DestroyLater>();
+        scene_registry.destroy_entities(view.begin(), view.end());
     }
 
 private:
